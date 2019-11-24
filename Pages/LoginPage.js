0fd0716415e5266a4fd951c73947739c41326e11
Hasan Berke <hbt19031@gmail.com>
2019-11-25 02:10:04 +0300

    Update LoginPage.js
    Co-Authored-By: talhaselimhan <talhaselimhan@users.noreply.github.com>

--- Pages/LoginPage.js
+++ Pages/LoginPage.js
@@ -4,10 +4,11 @@ import * as NB from "native-base";
 import * as RNE from "react-native-elements";
 import * as Navigation from "react-navigation";
 import {createStackNavigator} from "react-navigation-stack"
-import {createTabNavigator} from "react-navigation-tabs";
+import {createTabNavigator, createMaterialTopTabNavigator} from "react-navigation-tabs";
 import Colors from "../Config/Colors";
-import Background from "../Images/background.png"
-import Button from "../Components/Button"
+import Button from "../Components/Button";
+import Constants from "expo-constants";
+import now from "performance-now";
 
 
 class LandingPage extends React.Component{
@@ -18,7 +19,7 @@ class LandingPage extends React.Component{
                 <RN.View style={{flex:4, alignItems:"center", justifyContent:"center"}}>
                     <RN.Text style={{color:'#fcfcfc', fontSize:36, fontWeight:'bold', textAlign:'center', width:'80%'}}>Welcome to OFA</RN.Text>
                     <RN.Text style={{color:"#dedede", fontSize:14, textAlign:'center', width:'75%', margin:10}}>The best way to arrange matches and finding opponents</RN.Text>
-                    <Button title="LOGIN" onPress={() => this.props.navigation.navigate("Login")} containerStyle={{height:45, marginBottom:10}}/>
+                    <Button title="LOG IN" onPress={() => this.props.navigation.navigate("Login")} containerStyle={{height:45, marginBottom:10}}/>
                 </RN.View>
                 <RN.View style={{flex:3, alignItems:"center", justifyContent:"center", opacity:0.9}}>
                     <RN.Text style={{color:"#dedede", fontSize:14, textAlign:'center', width:'75%', margin:10}}>Continue with:</RN.Text>
@@ -41,10 +42,48 @@ class LandingPage extends React.Component{
 class LoginPage extends React.Component{
     render(){
         return(
-            <RN.View>
-                <RN.Text>Login Page</RN.Text>
-                <RN.Button title="Go Sign Up" onPress={() => this.props.navigation.navigate("Sign Up")}/>
-            </RN.View>
+            <RN.KeyboardAvoidingView style={{flex:1, backgroundColor:Colors.backgroundGreen, alignContent:'center', alignItems:'center', paddingTop:Constants.statusBarHeight}}>
+                <RN.View style={{flex:1, flexDirection:'row', marginTop:'5%'}}>
+                    <RN.View style={{flex:1, alignItems:'center'}}>
+                        <NB.Icon name="chevron-left" type="Entypo" onPress={() => this.props.navigation.pop()}/>
+                    </RN.View>
+                    <RN.View style={{flex:4, flexDirection:'row', justifyContent:'center'}}>
+                        <RN.TouchableOpacity style={styles.selectedStyle}>
+                            <RN.Text style={{fontSize:12, color:'#fff'}}>LOG IN</RN.Text>
+                        </RN.TouchableOpacity>
+                        <RN.TouchableOpacity style={styles.unSelectedStyle} 
+                                             onPress={() => this.props.navigation.navigate("Sign Up")}>
+                            <RN.Text style={{fontSize:12, color:'#fff'}}>SIGN UP</RN.Text>
+                        </RN.TouchableOpacity>
+                    </RN.View>
+                    <RN.View style={{flex:1}}/>
+                </RN.View>
+                <RN.View style={{flex:10, width:'90%'}}>
+                    <NB.Form>
+                        <NB.Item floatingLabel>
+                            <NB.Label >
+                                <NB.Icon style={{fontSize:20}} name="user" type="Feather"/>
+                                <RN.Text style={styles.labelStyle}> Username</RN.Text>
+                            </NB.Label>
+                            <NB.Input autoCapitalize="none" autoCompleteType="username"/>
+                        </NB.Item>
+                        <NB.Item floatingLabel style={{marginBottom:20}}>
+                            <NB.Label>
+                                <NB.Icon style={{fontSize:20}} name="lock" type="Entypo"/>
+                                <RN.Text style={styles.labelStyle}> Password</RN.Text>
+                            </NB.Label>
+                            <NB.Input autoCompleteType="password" autoCapitalize="none" secureTextEntry={true}/>
+                        </NB.Item>
+                    </NB.Form>
+                    <RN.Text style={styles.forgotPasswordStyle}>Forgot password ?</RN.Text>
+                </RN.View>
+                <RN.View style={{flex:2, alignContent:"flex-end", padding:10}}>
+                    <Button title="LOGIN"
+                        onPress={() => this.props.navigation.navigate("Login")} 
+                        containerStyle={{height:40, marginBottom:10, backgroundColor:'#fff'}}
+                        textStyle={{color:Colors.headerBackground}}/>
+                </RN.View>
+            </RN.KeyboardAvoidingView>
         )
     }
 }
@@ -53,17 +92,104 @@ class LoginPage extends React.Component{
 class SignUpPage extends React.Component{
     render(){
         return(
-            <RN.View>
-                <RN.Text>SignUp Page</RN.Text>
-            </RN.View>
+            <RN.KeyboardAvoidingView style={{flex:1, backgroundColor:Colors.backgroundGreen, alignContent:'center', alignItems:'center', paddingTop:Constants.statusBarHeight}}>
+                <RN.View style={{flex:1, flexDirection:'row', marginTop:'5%'}}>
+                    <RN.View style={{flex:1, alignItems:'center'}}>
+                        <NB.Icon name="chevron-left" type="Entypo" onPress={() => this.props.navigation.pop()}/>
+                    </RN.View>
+                    <RN.View style={{flex:4, flexDirection:'row', justifyContent:'center'}}>
+                        <RN.TouchableOpacity style={styles.unSelectedStyle}
+                                            onPress={() => this.props.navigation.navigate("Login")}>
+                            <RN.Text style={{fontSize:12, color:'#fff'}}>LOG IN</RN.Text>
+                        </RN.TouchableOpacity>
+                        <RN.TouchableOpacity style={styles.selectedStyle}>
+                            <RN.Text style={{fontSize:12, color:'#fff'}}>SIGN UP</RN.Text>
+                        </RN.TouchableOpacity>
+                    </RN.View>
+                    <RN.View style={{flex:1}}/>
+                </RN.View>
+                <RN.View  style={{flex:10, width:'90%'}}>
+                    <RN.ScrollView style={{maxHeight:'85%'}}>
+                        <NB.Form>
+                            <NB.Item floatingLabel>
+                                <NB.Label>
+                                    <NB.Icon style={{fontSize:16}} name="persona" type="Zocial"/>
+                                    <RN.Text style={styles.labelStyle}>  Full Name</RN.Text>
+                                </NB.Label>
+                                <NB.Input autoCapitalize="words" autoCompleteType="name"/>
+                            </NB.Item>
+                            <NB.Item floatingLabel>
+                                <NB.Label>
+                                    <NB.Icon style={{fontSize:20}} name="calendar" type="Feather"/>
+                                    <RN.Text style={styles.labelStyle}> Age</RN.Text>
+                                </NB.Label>
+                                <NB.Input keyboardType="numeric"/>
+                            </NB.Item>
+                            <NB.Item floatingLabel>
+                                <NB.Label>
+                                    <NB.Icon style={{fontSize:20}} name="email" type="MaterialCommunityIcons"/>
+                                    <RN.Text style={styles.labelStyle}> E-mail</RN.Text>
+                                </NB.Label>
+                                <NB.Input autoCapitalize="none" autoCompleteType="email"/>
+                            </NB.Item>
+                            <NB.Item floatingLabel>
+                                <NB.Label>
+                                    <NB.Icon style={{fontSize:20}} name="user" type="Feather"/>
+                                    <RN.Text style={styles.labelStyle}> Username</RN.Text>
+                                </NB.Label>
+                                <NB.Input autoCapitalize="none" autoCompleteType="username"/>
+                            </NB.Item>
+                            <NB.Item floatingLabel>
+                                <NB.Label>
+                                    <NB.Icon style={{fontSize:20}} name="lock" type="Entypo"/>
+                                    <RN.Text style={styles.labelStyle}> Password</RN.Text>
+                                </NB.Label>
+                                <NB.Input autoCompleteType="password" autoCapitalize="none" secureTextEntry={true}/>
+                            </NB.Item>
+                            <NB.Item floatingLabel>
+                                <NB.Label>
+                                    <NB.Icon style={{fontSize:20}} name="lock" type="Entypo"/>
+                                    <RN.Text style={styles.labelStyle}> Password Again</RN.Text>
+                                </NB.Label>
+                                <NB.Input autoCompleteType="password" autoCapitalize="none" secureTextEntry={true}/>
+                            </NB.Item>
+                        </NB.Form>
+                    </RN.ScrollView>
+                </RN.View>
+                <RN.View style={{flex:2, alignContent:"flex-end", padding:10}}>
+                    <Button title="LOGIN"
+                        onPress={() => this.props.navigation.navigate("Login")} 
+                        containerStyle={{height:40, marginBottom:10, backgroundColor:'#fff'}}
+                        textStyle={{color:Colors.headerBackground}}/>
+                </RN.View>
+            </RN.KeyboardAvoidingView>
         )
     }
 }
 
 
+const loginSignupNavigator = createMaterialTopTabNavigator(
+    {
+        "Login":{
+            screen:LoginPage,
+        },
+        "Sign Up":{
+            screen:SignUpPage
+        }
+    },
+    {
+        swipeEnabled:true,
+        tabBarOptions:{
+            style:{display:'none'}
+        },
+        lazy:true
+    }
+
+)
+
 const LandingNavigator = createStackNavigator(
     {
-        "Home": {
+        "Home":{
                 screen:LandingPage, 
                 title:"Home",
                 navigationOptions:{
@@ -71,20 +197,45 @@ const LandingNavigator = createStackNavigator(
                 }
         },
         "Login":{
-                screen:LoginPage,
+                screen:loginSignupNavigator,
                 title:'Login',
                 navigationOptions:{
                     header:null
                 }
-        },
-        "Sign Up":{
-                    screen:SignUpPage,
-                    title:'Sign Up',
-                    navigationOptions:{
-                        header:null
-                    }
-        }
+        }    
     },
 );
+
+const styles = RN.StyleSheet.create({
+    labelStyle: {
+      color:'#ccc', 
+      fontSize:14, 
+      letterSpacing:1.5
+    },
+    selectedStyle:{
+        backgroundColor:Colors.headerBackground, 
+        height:30, 
+        paddingVertical:5, 
+        paddingHorizontal:15, 
+        alignContent:'center',
+        justifyContent:'center', 
+        borderRadius:15
+    },
+    unSelectedStyle:{
+        backgroundColor:"transparent", 
+        height:30, 
+        paddingVertical:5, 
+        paddingHorizontal:15, 
+        alignContent:'center',
+        justifyContent:'center', 
+        borderRadius:15
+    },
+    forgotPasswordStyle:{
+        textAlign:'center', 
+        color:"#ccc", 
+        fontSize:12
+    },
+  });
+
 
 export default Navigation.createAppContainer(LandingNavigator);
