a9d18a02e4022d37ba1d1dc2c654aaf5cd3f69bc
TalhaSelimhan <TalhaSelimhan@users.noreply.github.com>
2019-12-19 23:11:33 +0300

    Drawer edited

--- Pages/TabNavigator.js
+++ Pages/TabNavigator.js
@@ -1,21 +1,23 @@
 
 import * as React from "react";
-import {View,Text} from "react-native";
+import {View,Text,TouchableOpacity,SafeAreaView,Dimensions,ScrollView} from "react-native";
 import * as NB from "native-base";
 import {createAppContainer} from "react-navigation";
 import Colors from "../Config/Colors";
 import Button from "../Components/Button";
 import Constants from "expo-constants";
-import now from "performance-now";
 import PlayerProfile from "./PlayerProfile";
-import CreateTeam from "./CreateTeam";
-import TeamProfile from "./TeamProfile";
 import ListMatches from "./ListMatches";
 import ListTeams from "./ListTeams";
 import ListFields from "./ListFields";
 import {createStackNavigator} from 'react-navigation-stack'
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createDrawerNavigator, DrawerItems} from 'react-navigation-drawer';
+import Firebase from "../Config/Firebase";
 
+
+const Screen = Dimensions.get("screen");
+const height = Screen.height;
+const width = Screen.width;
 
 
 const PlayerPage = createStackNavigator({
@@ -52,6 +54,21 @@ const TeamsPage = createStackNavigator({
     }
   });
 
+  const CustomDrawerComponent= (props) =>{
+      return(
+        <View style={{flex:1}}>
+          <View style={{height:250,backgroundColor:Colors.sideBarHeaderBackground}}>
+            <TouchableOpacity style={{paddingTop:40}} onPress={async ()=>await Firebase.auth().signOut()}>
+              <Text style={{fontSize:24,color:"white"}}>Sign Out</Text>
+            </TouchableOpacity>
+          </View>
+          <ScrollView style={{backgroundColor:Colors.backgroundGreen}}>
+            <DrawerItems  {...props}  itemStyle={{borderRadius:100,margin:5,alignSelf:"flex-start"}} activeBackgroundColor={Colors.postBackground} />
+          </ScrollView>
+
+        </View>
+      );
+  }
   
   const MyDrawerNavigator = createDrawerNavigator({
     "Player Profile": {
@@ -65,8 +82,16 @@ const TeamsPage = createStackNavigator({
     },
     "Last Matches": {
         screen: MatchesPage,
+    }
+  },
+  {
+    contentComponent:CustomDrawerComponent,
+    contentOptions: {
+      activeTintColor: "white",
     },
-  });
+    drawerWidth:width*0.65
+  }
+  );
   
 export default MyApp = createAppContainer(MyDrawerNavigator);
 
