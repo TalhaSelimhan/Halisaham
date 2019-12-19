bbfb21b4a48a7930b9af5b73dff407b67b88be1c
Hasan Berke <hbt19031@gmail.com>
2019-12-19 18:15:49 +0300

    Team List firebase connection

--- Pages/ListTeams.js
+++ Pages/ListTeams.js
@@ -8,63 +8,108 @@ const window = RN.Dimensions.get("window");
 const width = window.width;
 const height = window.height;
 const statusBarHeight = Constants.statusBarHeight;
+import {createStackNavigator} from "react-navigation-stack"
+import {createAppContainer} from "react-navigation";
+import Firebase from '../Config/Firebase';
+require('firebase/firestore');
+import TeamProfile from "./TeamProfile";
 
 
 class Item extends React.Component{
     render(){
-        var {labels, post, title} = this.props;
+        var {team, navigation} = this.props;
+        console.log(team);
         return(
-            <RN.View style={styles.MatchPostView}>
-                <RN.ImageBackground source={{uri:'https://www.elsetge.cat/myimg/f/91-914400_soccer-s-s-c-napoli-logo-ssc-napoli.jpg'}} style={{width:width*.9, height:height*.2}} resizeMode="cover">
+            <RN.TouchableOpacity style={styles.MatchPostView} onPress={() => navigation.navigate('TeamProfile', {uid:team.id})} >
+                <RN.ImageBackground source={{uri:team.photourl}} style={{width:width*.9, height:height*.2}} resizeMode="cover">
                     <RN.View style={{opacity:.4, backgroundColor:'#000', position:"absolute", top:0, right:0, left:0, bottom:0}}/>
                     <RN.View style={styles.PostHeader}>
                         <RN.View style={{opacity:.6, backgroundColor:'#000', position:"absolute", top:0, right:0, left:0, bottom:0}}/>
-                        <RN.Text style={{color:'#fff', fontSize:16, letterSpacing:2, fontWeight:'700'}}>{title}</RN.Text>
+                        <RN.Text style={{color:'#fff', fontSize:16, letterSpacing:2, fontWeight:'700'}}>{team.name}</RN.Text>
                     </RN.View>  
                     <RN.View style={{flex:5, flexDirection:'row', alignContent:'center', alignItems:'center', padding:10}}>
                         <RN.View style={{flex:1}}>
-                            {labels.map(label => <RN.Text style={{fontSize:12,fontWeight:'600', color:'#fff', letterSpacing:2}}>{label}</RN.Text>)}
+                            <RN.Text style={{fontSize:12,fontWeight:'600', color:'#fff', letterSpacing:2}}>Total Matches</RN.Text>
+                            <RN.Text style={{fontSize:12,fontWeight:'600', color:'#fff', letterSpacing:2}}>City</RN.Text>
+                            <RN.Text style={{fontSize:12,fontWeight:'600', color:'#fff', letterSpacing:2}}>Founded</RN.Text>
+                            <RN.Text style={{fontSize:12,fontWeight:'600', color:'#fff', letterSpacing:2}}>Contact</RN.Text>
                         </RN.View>
                         <RN.View style={{flex:1}}>
-                            {post.map(info => <RN.Text style={{fontSize:12, textAlign:'right', fontWeight:'300', color:'#ccc', letterSpacing:2}}>{info}</RN.Text>)}
+                            <RN.Text style={{fontSize:12, textAlign:'right', fontWeight:'300', color:'#ccc', letterSpacing:2}}>{team.totalmatches}</RN.Text>
+                            <RN.Text style={{fontSize:12, textAlign:'right', fontWeight:'300', color:'#ccc', letterSpacing:2}}>{team.city}</RN.Text>
+                            <RN.Text style={{fontSize:12, textAlign:'right', fontWeight:'300', color:'#ccc', letterSpacing:2}}>{team.founded}</RN.Text>
+                            <RN.Text style={{fontSize:12, textAlign:'right', fontWeight:'300', color:'#ccc', letterSpacing:2}}>{team.contact}</RN.Text>
                         </RN.View>
                     </RN.View>
                     
                 </RN.ImageBackground>
-            </RN.View>
+            </RN.TouchableOpacity>
         )
     }
 }
 
-var team = {
-    name: 'Napoli SCF',
-    location:'Maslak/Istanbul',
-    totalmatch:36,
-    founded: '13.08.2019',
-    contact: '+90 544 444 44 44'
-}
 
-var labels = ['Total Matches', 'City', 'Founded', 'Contact']
-var post = [team.totalmatch, team.location, team.founded, team.contact]
-var title = team.name;
-
-export default class ListTeams extends React.Component{
+class ListTeams extends React.Component{
+    state={
+        teams:[],
+        loaded:false
+    }
+    async loadTeams(){
+        let that = this;
+        let teams = [];
+        let teamsRef = Firebase.firestore().collection('teams');
+        await teamsRef.get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+                let team = doc.data();
+                var d = new Date( team.founded );
+                team.founded = d.getFullYear();
+                team.id = doc.id;
+                teams.push(team);
+            })
+        })
+        this.setState({teams:teams, loaded:true});
+    }
+    async componentWillMount(){
+        await this.loadTeams();
+    }
     render(){
+        let {teams, loaded} = this.state;
+        console.log(teams)
         return(
             <RN.View style={styles.ListMatchesView}>
                 <RN.View style={{height:height*.2, width:width, alignItems:'center', justifyContent:'center', padding:5}}>
                     <RN.Text style={{fontSize:24, color:'#fff', elevation:4, shadowColor:'black', shadowOpacity:.9, shadowOffset:{width:0, height:2}, letterSpacing:2, fontWeight:'800'}}>Teams List</RN.Text>
                 </RN.View>
-                <RN.ScrollView contentContainerStyle={{height:height*.7}}>
-                    <Item post = {post} labels={labels} title={title}/>
-                    <Item post = {post} labels={labels} title={title}/>
-                </RN.ScrollView>
+                {loaded ? <RN.FlatList
+                    data={teams}
+                    renderItem={(item) => <Item team = {item.item} navigation={this.props.navigation}/>}
+                    keyExtractor={(item) => item.id}
+                    
+                /> : <RN.View/>}
+                
                 
             </RN.View>
         )
     }
 }
 
+
+const ListStack = createStackNavigator({
+    List:{
+        screen:ListTeams,
+        navigationOptions:{
+            header:null,
+        }
+    },
+    TeamProfile:{
+        screen:TeamProfile,
+        navigationOptions:{
+            header:null
+        }
+    }
+})
+
+export default createAppContainer(ListStack);
 
 const styles = RN.StyleSheet.create({
     MatchPostView:{
