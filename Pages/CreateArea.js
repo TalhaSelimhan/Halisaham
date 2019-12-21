ccd1d1ba8d9cb8af6bf9fd6a23e38c895fbca6f7
TalhaSelimhan <TalhaSelimhan@users.noreply.github.com>
2019-12-21 16:40:50 +0300

    createArea Page

--- Pages/CreateArea.js
+++ Pages/CreateArea.js
@@ -15,7 +15,9 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
-
+import Firebase from "../Config/Firebase";
+require('firebase/firestore');
+const areaRef = Firebase.firestore().collection('areas');
 
 export default class CreateArea extends React.Component{
     state={
@@ -30,6 +32,8 @@ export default class CreateArea extends React.Component{
         },
         loaded:false,
         x:{},
+        price:null,
+        locationAdded:false,
     }
     setLocation(){
         this.setState({coordinates:this.state.location.coords})
@@ -66,21 +70,20 @@ export default class CreateArea extends React.Component{
         let location = await Location.getCurrentPositionAsync({});
         
         this.setState({location:location.coords});
-        this.setState({location:{...this.state.location,
+        await this.setState({location:{...this.state.location,
             latitudeDelta:0.001,
             longitudeDelta:0.001},
-            loaded:true});
-
-        
+            loaded:true,});
+            this.setState({x:{longitude:location.longitude,latitude:location.latitude}});
       };
     
     
     _pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        mediaTypes: "Images",
         allowsEditing: true,
         aspect: [4, 3],
-        quality: 1
+        quality: 0.7
         });
 
         console.log(result);
@@ -89,6 +92,55 @@ export default class CreateArea extends React.Component{
         this.setState({ image: result.uri });
         }
     }
+
+
+    async resimYukle(uri, resimYolu) {
+        const blob = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = function() {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function(e) {
+            console.log(e);
+            reject(new TypeError('Network request failed'));
+          };
+          xhr.responseType = 'blob';
+          xhr.open('GET', uri, true);
+          xhr.send(null);
+        });
+        const ref = Firebase.storage().ref().child(resimYolu);
+        const snapshot = await ref.put(blob);
+      
+        blob.close();
+        let url = await snapshot.ref.getDownloadURL();
+        return url;
+      }
+
+
+
+    createArea(){
+        let areaid=null;
+        areaRef.add({
+            city:"Istanbul",
+            name:this.state.areaName,
+            contact:this.state.contactnumber,
+            latitude:this.state.x.latitude,
+            longitude:this.state.x.longitude,
+            price:this.state.price,
+            rating:2.5,
+            ratingcount:0,
+            votes:[],
+        }).then(async (doc)=>{
+            RN.Alert.alert(doc.id);
+            let imageUri= await this.resimYukle(this.state.image,"/areas/"+doc.id);
+            areaRef.doc(doc.id).update({
+                photourl:imageUri
+            });    
+        });
+        
+
+    }
+
     render(){
         let image = this.state.image
         return(
@@ -107,6 +159,12 @@ export default class CreateArea extends React.Component{
                                 <RN.Text style={styles.labelStyle}>  Contact Number</RN.Text>
                             </NB.Label>
                             <NB.Input keyboardType="number-pad" value={this.state.contactnumber} onChangeText={(contactnumber)=>this.setState({contactnumber})} style={{fontSize:18,color:"white"}} autoCapitalize="none"/>
+                        </NB.Item>
+                        <NB.Item floatingLabel >
+                            <NB.Label>
+                                <RN.Text style={styles.labelStyle}>  Price</RN.Text>
+                            </NB.Label>
+                            <NB.Input keyboardType="number-pad" value={this.state.price} onChangeText={(price)=>this.setState({price})} style={{fontSize:18,color:"white"}} autoCapitalize="none"/>
                         </NB.Item>
                         <NB.Label style={{margin:20,marginTop:50,alignSelf:"center"}} onPress={this._pickImage}>
                             <NB.Icon style={{fontSize:20,color:"#ccc"}} name="photo" type="MaterialIcons"/>
@@ -145,7 +203,7 @@ export default class CreateArea extends React.Component{
                     </NB.Form>
                 </RN.ScrollView>
                 <RN.View style={{flex:1,justifyContent:"center" ,marginBottom:20,alignItems:"center"}}>
-                    <Button title="CREATE TEAM" />
+                    <Button title="CREATE AREA" onPress={()=>this.createArea()} />
                 </RN.View>
             </RN.View>
         )
